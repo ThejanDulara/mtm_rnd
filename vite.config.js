0a43d8d1686aae4e2103591a018560af5872d374
Thejan Dulara <thejangammanpila@gmail.com>
2025-11-07 11:17:02 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Any request starting with /api will be forwarded to your Flask backend
+      '/api': {
+        target: 'https://mtmbackend-production.up.railway.app', // 👈 your Flask backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
